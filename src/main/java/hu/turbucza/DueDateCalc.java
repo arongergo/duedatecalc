586f86cc2a9ca8abe6d02d9a635e464448502da4
turbucz.aron <turbucz.aron@nextent.hu>
2019-11-13 12:28:04 +0100

    CODE in work day (and some additive tests)

--- src/main/java/hu/turbucza/DueDateCalc.java
+++ src/main/java/hu/turbucza/DueDateCalc.java
@@ -5,6 +5,23 @@ import java.time.LocalDateTime;
 public class DueDateCalc {
 
     public static LocalDateTime calculate(LocalDateTime submitDate, Float turnaroundHours) {
-        return null;
+        checkSubmitDate(submitDate);
+        checkTurnaroundHours(turnaroundHours);
+        return submitDate.plusHours(turnaroundHours.intValue());
+    }
+
+    private static void checkTurnaroundHours(Float turnaroundHours) {
+        if(turnaroundHours == null) {
+            throw new IllegalArgumentException("Turnaround hours must not be null!");
+        }
+        if(turnaroundHours < 0) {
+            throw new IllegalArgumentException("Turnaround hours must not be positive!");
+        }
+    }
+
+    private static void checkSubmitDate(LocalDateTime submitDate) {
+        if(submitDate == null) {
+            throw new IllegalArgumentException("Submit date/time must not be null!");
+        }
     }
 }
--- src/test/java/hu/turbucza/DueDateCalcTest.java
+++ src/test/java/hu/turbucza/DueDateCalcTest.java
@@ -11,6 +11,29 @@ public class DueDateCalcTest {
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
+    @Test(expected=IllegalArgumentException.class)
+    public void calcNullDate() {
+        DueDateCalc.calculate(null, 4f);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void calcNullHours() {
+        // given
+        String submitDate = "2019-11-03 09:00";
+
+        // when
+        DueDateCalc.calculate(toDate(submitDate), null);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void calcNegativeHours() {
+        // given
+        String submitDate = "2019-11-03 09:00";
+
+        // when
+        DueDateCalc.calculate(toDate(submitDate), -12f);
+    }
+
     @Test
     public void calcInWorkday() {
         // given
