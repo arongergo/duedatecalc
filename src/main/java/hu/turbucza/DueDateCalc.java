8c4bd1ad4eaacd960fab5816c72967befbd372f9
turbucz.aron <turbucz.aron@nextent.hu>
2019-11-13 12:01:51 +0100

    TEST in work day

--- src/main/java/hu/turbucza/DueDateCalc.java
+++ src/main/java/hu/turbucza/DueDateCalc.java
@@ -1,4 +1,10 @@
 package hu.turbucza;
 
+import java.time.LocalDateTime;
+
 public class DueDateCalc {
+
+    public static LocalDateTime calculate(LocalDateTime submitDate, Float turnaroundHours) {
+        return null;
+    }
 }
--- src/test/java/hu/turbucza/DueDateCalcTest.java
+++ src/test/java/hu/turbucza/DueDateCalcTest.java
@@ -1,5 +1,29 @@
 package hu.turbucza;
 
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.Assert.assertEquals;
+
 public class DueDateCalcTest {
 
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    @Test
+    public void calcInWorkday() {
+        // given
+        String submitDate = "2019-11-03 09:00";
+
+        // when
+        LocalDateTime actual = DueDateCalc.calculate(toDate(submitDate), 6f);
+
+        // then
+        assertEquals(toDate("2019-11-03 15:00"), actual);
+    }
+
+    private LocalDateTime toDate(String parseFrom) {
+        return LocalDateTime.parse(parseFrom, formatter);
+    }
 }
